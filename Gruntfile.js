1319355e8342299f4efcc16a8c7630493adb0dcc
Karthik Vasudevan <mithralaya@gmail.com>
2015-08-06 14:39:39 +0000

    added grunt and bower. installed required libs for the project

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Karthik on 06/08/15.
+ */
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'public/stylesheets/style.css': 'public/stylesheets/style.sass'
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                sourceMap: true
+            },
+            build: {
+                src: "public/javascripts/global.js",
+                dest: "public/javascripts/global.min.js"
+            }
+        },
+
+        concat: {
+            options: {
+                separator: '\n'
+            },
+            js: {
+                src: ['bower_components/jquery/dist/jquery.min.js'
+                    , 'bower_components/bootstrap/dist/js/bootstrap.min.js'
+                    , 'node_modules/angular/angular.min.js'
+                    , 'public/javascripts/global.min.js'],
+
+                dest: 'public/javascripts/lib.js'
+            },
+            css: {
+                src: ['bower_components/bootstrap/dist/css/bootstrap.min.css'
+                    , 'bower_components/animate.css/animate.min.css'
+                    , 'bower_components/ionicons/css/ionicons.min.css'
+                    , 'public/stylesheets/style.css'],
+
+                dest: 'public/stylesheets/lib.css'
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['sass', 'uglify', 'concat']);
+
+};
